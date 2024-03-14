64bca8e45f11cca61c3bfce041bca4452c939cc7
Naimul02 <naimulislum39@gmail.com>
2024-03-14 11:32:22 +0600

    commit no 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import { useState } from 'react'
+import {  useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import BookMarks from './components/Bookmarks/BookMarks'
 import Header from './components/Header/Header'
 
 function App() {
+      const [bookmarks , setBookmarks] = useState([]);
 
+
+      const handleAddToBookmark = blog => {
+          const newBookmarks = [...bookmarks , blog];
+          setBookmarks(newBookmarks);
+      }
 
   return (
     <>
       <Header></Header>
-      <div className="md:flex">
-      <Blogs></Blogs>
-      <BookMarks></BookMarks>
+      <div className="md:flex max-w-6xl mx-auto">
+      <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs>
+      <BookMarks
+        bookmarks={bookmarks}
+      ></BookMarks>
 
       </div>
       
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,11 @@
-
+import { IoBookmarkOutline } from "react-icons/io5";
 import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
+const Blog = ({blog , handleAddToBookmark}) => {
   const {title , cover ,reading_time , author , author_img , posted_date ,hashtags } = blog;
   return (
-    <div>
-          <img src={cover} alt={`Cover picture of the title ${title}`} />
-          <div className="flex justify-between">
+    <div className="mb-20">
+          <img className="w-full mb-8" src={cover} alt={`Cover picture of the title ${title}`} />
+          <div className="flex justify-between mb-4">
               <div className="flex">
                     <img className="w-14"src={author_img} alt="" />
                     <div className="ml-6">
@@ -14,8 +14,9 @@ const Blog = ({blog}) => {
 
                     </div>
               </div>
-              <div>
+              <div className="flex items-center">
                     <span>{reading_time} min read</span>
+                    <button onClick={() => handleAddToBookmark(blog)} className="ml-2 text-red-600 text-2xl"><IoBookmarkOutline /></button>
               </div>
           </div>
           <h2 className="text-4xl">{title}</h2>
@@ -29,7 +30,9 @@ const Blog = ({blog}) => {
 };
 
 Blog.propTypes = {
-    blog : PropTypes.object.isRequired
+    blog : PropTypes.object.isRequired,
+    handleAddToBookmark : PropTypes.func
+
 }
 
 export default Blog;
